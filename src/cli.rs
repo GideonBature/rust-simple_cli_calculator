ef8a6a0f73fca8c7f3cff85156f5e9d2163788fa
GideonBature <infoaboutgideon@gmail.com>
2024-07-08 21:13:06 +0100

    restructure readline to module

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,11 @@
-/* pub mod readline_mod {
-    pub fn readline_fn() -> String {
-        let input = rprompt::prompt_reply("First Number: ").unwrap();
+pub mod cli_readline {
+    pub fn readline_float(message: String) -> f32 {
+        let prompt: f32 = rprompt::prompt_reply(format!("{message}")).expect("Invalid Input").trim().parse().unwrap();
+        prompt
+    }
+
+    pub fn readline_string(message: String) -> String {
+        let prompt: String = rprompt::prompt_reply(format!("{message}")).unwrap();
+        prompt
     }
 }
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use rprompt;
+mod cli;
+
+pub use cli::cli_readline::{readline_float, readline_string};
 
 fn main() {
     let num1: f32 = readline_float(String::from("Enter first number: "));
@@ -8,7 +10,7 @@ fn main() {
     let num2: f32 = readline_float(String::from("Enter second number: "));
     println!("{}", num2);
 }
-
+/*
 fn readline_float(message: String) -> f32 {
     let prompt: f32 = rprompt::prompt_reply(format!("{message}")).expect("Sorry, enter a valid number").parse().unwrap();
     prompt
@@ -18,3 +20,4 @@ fn readline_string(message: String) -> String {
     let prompt: String = rprompt::prompt_reply(format!("{message}")).unwrap();
     prompt
 }
+*/
