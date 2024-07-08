916a938fe61039de5f40378003314e626bf09655
GideonBature <infoaboutgideon@gmail.com>
2024-07-08 20:11:57 +0100

    readline function working

---
+++ src/cli.rs
@@ -0,0 +1,6 @@
+/* pub mod readline_mod {
+    pub fn readline_fn() -> String {
+        let input = rprompt::prompt_reply("First Number: ").unwrap();
+    }
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rprompt;
+
 fn main() {
-    println!("Hello, world!");
+    let num1: f32 = readline_float(String::from("Enter first number: "));
+    println!("{}", num1);
+    let operation: String = readline_string(String::from("Enter the operator(+, -, x, /, %): "));
+    println!("{}", operation);
+    let num2: f32 = readline_float(String::from("Enter second number: "));
+    println!("{}", num2);
+}
+
+fn readline_float(message: String) -> f32 {
+    let prompt: f32 = rprompt::prompt_reply(format!("{message}")).expect("Sorry, enter a valid number").parse().unwrap();
+    prompt
+}
+
+fn readline_string(message: String) -> String {
+    let prompt: String = rprompt::prompt_reply(format!("{message}")).unwrap();
+    prompt
 }
